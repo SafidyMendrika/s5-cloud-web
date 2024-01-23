a1027bdcaa476656bfb81ebd7411a2225c4bafb0
RAKOTOARIVONY Judichael Tokiniaina <111460237+RJToky@users.noreply.github.com>
2024-01-23 22:45:35 +0300

    [Add] page commissions

--- src/pages/back-office/Commission.jsx
+++ src/pages/back-office/Commission.jsx
@@ -1,5 +1,62 @@
 const Commission = () => {
-  return <h3>Commissions</h3>;
+  return (
+    <>
+      <h3 className="mb-4 text-gray">Commissions</h3>
+      <div className="row">
+        <div className="col-md-5">
+          <div className="card">
+            <div className="card-body">
+              <div className="row alig n-items-start">
+                <div className="col-8">
+                  <h5 className="card-title mb-9 fw-semibold">
+                    Commission actuelle
+                  </h5>
+                  <h4
+                    className="fw-semibold"
+                    style={{ color: "var(--bs-muted)" }}
+                  >
+                    20%
+                  </h4>
+                </div>
+                <div className="col-4">
+                  <div className="d-flex justify-content-end">
+                    <div className="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
+                      <i className="ti ti-currency-dollar fs-6"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-5">
+          <div className="card w-100">
+            <div className="card-body p-4">
+              <h5 className="card-title fw-semibold mb-4">
+                Modifier la commission
+              </h5>
+              <form className="row">
+                <div className="col-12 mb-3">
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Entrer la nouvelle commission"
+                  />
+                </div>
+                <div className="col-12">
+                  <button className="btn btn-primary w-100 py-8 fs-4 rounded-2">
+                    Modifier
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Commission;
--- src/pages/back-office/Statistique.jsx
+++ src/pages/back-office/Statistique.jsx
@@ -142,36 +142,6 @@ const Statistique = () => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-12">
-              <div className="card">
-                <div className="card-body">
-                  <div className="row alig n-items-start">
-                    <div className="col-8">
-                      <h5 className="card-title mb-9 fw-semibold">
-                        {" "}
-                        Monthly Earnings{" "}
-                      </h5>
-                      <h4 className="fw-semibold mb-3">$6,820</h4>
-                      <div className="d-flex align-items-center pb-1">
-                        <span className="me-2 rounded-circle bg-light-danger round-20 d-flex align-items-center justify-content-center">
-                          <i className="ti ti-arrow-down-right text-danger"></i>
-                        </span>
-                        <p className="text-dark me-1 fs-3 mb-0">+9%</p>
-                        <p className="fs-3 mb-0">last year</p>
-                      </div>
-                    </div>
-                    <div className="col-4">
-                      <div className="d-flex justify-content-end">
-                        <div className="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                          <i className="ti ti-currency-dollar fs-6"></i>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div id="earning"></div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
