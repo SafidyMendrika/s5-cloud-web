c9230c974bda238bda04e155cc3bebe7eab6546e
RAKOTOARIVONY Judichael Tokiniaina <111460237+RJToky@users.noreply.github.com>
2024-01-25 19:26:45 +0300

    Update UrlContext.jsx

--- src/context/UrlContext.jsx
+++ src/context/UrlContext.jsx
@@ -1,1 +1,5 @@
-export const API_URL = "http://172.20.10.2:7979/api";
+// Local
+// export const API_URL = "http://172.20.10.2:7979/api";
+
+// Online
+export const API_URL = "https://s5-cloud-api.up.railway.app/api";
